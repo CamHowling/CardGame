25ce426bb9cccbcfe508fc3aa7c853938e5399ca
CamHowling <>
2023-06-18 15:17:45 +1000

    remove card intialization (to be moved to a migration)

--- Database/DatabaseConfig.cs
+++ Database/DatabaseConfig.cs
@@ -10,23 +10,12 @@ namespace CardGame.Database
             using (var serviceScope = application.Services.CreateScope())
             {
                 Migrate(serviceScope.ServiceProvider.GetService<DatabaseContext>());
-                InitializeData(serviceScope.ServiceProvider.GetService<DatabaseContext>());
             }
         }
 
         private static void Migrate(DatabaseContext context)
         {
             context.Database.Migrate();
-        }
-
-        private static void InitializeData(DatabaseContext context) 
-        { 
-            if (!context.Cards.Any())
-            {
-                //Populate the card list
-            }
-
-            context.SaveChanges();
         }
     }
 }
