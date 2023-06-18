f777e642b7990b9debb40cc971179edecf654eca
CamHowling <>
2023-06-18 14:59:53 +1000

    #8 simple db context class added

---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using CardGame.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardGame.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Deck> Decks { get; set; }
+        public DbSet<Pile> Piles { get; set; }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
