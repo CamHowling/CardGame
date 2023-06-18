342ba9ebd1dea1e2a07e4bde6f306accb3d42e3e
CamHowling <>
2023-06-18 15:17:16 +1000

    #12 DB intializer class

---
+++ Database/DatabaseConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CardGame.Database
+{
+    public static class DatabaseConfig
+    {
+        public static void Initialize(WebApplication application)
+        {
+            using (var serviceScope = application.Services.CreateScope())
+            {
+                Migrate(serviceScope.ServiceProvider.GetService<DatabaseContext>());
+                InitializeData(serviceScope.ServiceProvider.GetService<DatabaseContext>());
+            }
+        }
+
+        private static void Migrate(DatabaseContext context)
+        {
+            context.Database.Migrate();
+        }
+
+        private static void InitializeData(DatabaseContext context) 
+        { 
+            if (!context.Cards.Any())
+            {
+                //Populate the card list
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
