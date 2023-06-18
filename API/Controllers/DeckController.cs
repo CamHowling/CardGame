c564a2419856858bd5e2d340f0c4d6b27322d424
CamHowling <>
2023-06-18 18:16:13 +1000

    #13 - Initial controller layout/api calls

--- API/Controllers/DeckController.cs
+++ API/Controllers/DeckController.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using CardGame.API.Models;
+using CardGame.Domain.Models;
 using CardGame.Domain.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +28,58 @@ namespace CardGame.API.Controllers
             _Mapper = mapper;
         }
 
+        /*
+         Actions:
+            Submit Guess / Draw Card
+                Correct guess +1 point
+                Incorrect guess -1 point
+            End Game
+                //Can assume that score tracking will be handled by cookies...
+                //this will set deck as inactive
+            StartGame/NewDeck
+                Get all the cards....
 
+            GetDeck
+                //Get active deck, return invalid if the deck has been finished
+         */
+
+        [HttpGet]
+        public ActionResult<DeckModel> NewGame()
+        {
+            var drawPile = new Pile();
+            var discard = new Pile();
+
+            var deck = new Deck();
+            _DeckRepository.CreateDeck(deck);
+            _DeckRepository.SaveChanges();
+
+            var model = _Mapper.Map<Deck>(deck);
+            return Ok(model);
+        }
+
+        [HttpGet]
+        public ActionResult<DeckModel> GetDeck(int id)
+        {
+            var deck = _DeckRepository.GetDeckById(id);
+            var model = _Mapper.Map<Deck>(deck);
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public ActionResult<CardModel> SubmitGuess(bool isHigher, int deckId)
+        {
+            var deck = _DeckRepository.GetDeckById(deckId);
+            var piles = _PileRepository.GetDeckPiles(deck);
+            var drawPile = piles.FirstOrDefault(p => p.IsDrawPile);
+            var discardPile = piles.FirstOrDefault(p => p.IsDiscardPile);
+
+            //remove first item from draw pile. 
+            //Check against last item in discard pile.
+            //Move to discard pile.
+            //Save both piles.
+            //Get the card model.
+            //Return the card model 
+
+        }
     }
 }
